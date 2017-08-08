469a34405c36834faf8d96d07cd7600af079a830
Alejandro Napoles <alejandro@alejandronapoles.com>
2017-08-08 17:35:24 +0200

    WS-2017.2 <Alejandro@Work Create baseRefactoring.xml, IntelliLang.xml, ide.general.xml, git.xml, project.default.xml, diff.xml, other.xml, terminal.xml, Default.xml, code.style.schemes, find.xml, ui.lnf.xml, bayuk.xml, filetypes.xml, keymap.xml, code.style.schemes.xml, colors.scheme.xml, airbnb.xml, abbreviations.xml, Default.xml, JavaScript.xml, laf.xml, WarmNeon_2_.icls, React Pure Component.js, Default _1_.xml, web-browsers.xml, React Component.js, editor.xml, debugger.xml

---
+++ fileTemplates/React Component.js
@@ -0,0 +1,14 @@
+import React, {Component} from "react";
+
+class $COMPONENT extends Component {
+  render() {
+    return (
+      <div>$END</div>
+    );
+  }
+}
+
+$COMPONENT .propTypes = {};
+$COMPONENT .defaultProps = {};
+
+export default $COMPONENT;
---
+++ fileTemplates/React Pure Component.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const $COMPONENT  = ($PARAMETER) => {
+  return (
+    <div>$END</div>
+  );
+};
+
+$COMPONENT .propTypes = {};
+$COMPONENT .defaultProps = {};
+
+export default $COMPONENT;
